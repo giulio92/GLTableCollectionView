411cc9003b98619c5cb6042a46335f46598f9d34
Giulio Lombardo <giulio.lombardo@gmail.com>
2016-11-24 13:09:14 +0100

    - Reduced footer height from in Main.Storyboard - Reduced footer height from code to overcome Apple's limitations about UITableViewFooterViewHeight being greater than 0

--- GLTableCollectionView/GLTableCollectionViewController.swift
+++ GLTableCollectionView/GLTableCollectionViewController.swift
@@ -46,40 +46,17 @@ class GLTableCollectionViewController: UITableViewController, UICollectionViewDa
 		return "Section: " + section.description
 	}
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
+	override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+		return 88
+	}
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
+	override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+		return 28
+	}
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
+	override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+		return 0.0001
+	}
 
 	// MARK: <UITableView Delegate>
 
