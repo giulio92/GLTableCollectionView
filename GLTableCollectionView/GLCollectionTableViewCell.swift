79149b2d1cf67aebae83e1c51051a3e73f7e72e6
Giulio Lombardo <giulio.lombardo@gmail.com>
2016-11-24 10:45:28 +0100

    - Project setup - Added a .gitignore

---
+++ GLTableCollectionView/GLCollectionTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  GLCollectionTableViewCell.swift
+//  GLTableCollectionView
+//
+//  Created by Giulio Lombardo on 24/11/16.
+//  Copyright © 2016 Giulio Lombardo. All rights reserved.
+//
+
+import UIKit
+
+class GLCollectionTableViewCell: UITableViewCell {
+	override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
---
+++ GLTableCollectionView/GLTableCollectionViewController.swift
@@ -0,0 +1,107 @@
+//
+//  GLTableCollectionViewController.swift
+//  GLTableCollectionView
+//
+//  Created by Giulio Lombardo on 24/11/16.
+//  Copyright © 2016 Giulio Lombardo. All rights reserved.
+//
+
+import UIKit
+
+class GLTableCollectionViewController: UITableViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Uncomment the following line to preserve selection between presentations
+		// self.clearsSelectionOnViewWillAppear = false
+
+		// Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+		// self.navigationItem.rightBarButtonItem = self.editButtonItem()
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+	// MARK: <UITableView Data Source>
+
+	override func numberOfSections(in tableView: UITableView) -> Int {
+		return 1
+	}
+
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 2
+	}
+
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+		// Configure the cell...
+
+		return cell
+	}
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+	// MARK: <UICollectionView Data Source>
+
+	func numberOfSections(in collectionView: UICollectionView) -> Int {
+		return 1
+	}
+
+	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		return 1
+	}
+
+	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "", for: indexPath)
+
+		// Configure the cell...
+
+		return cell
+	}
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
