9eaf7298f57b2495443b59c1497f497ba89d0539
Giulio Lombardo <giulio.lombardo@gmail.com>
2016-12-02 16:34:45 +0100

    - Updated comments formatting

--- GLTableCollectionView/GLTableCollectionViewController.swift
+++ GLTableCollectionView/GLTableCollectionViewController.swift
@@ -26,10 +26,12 @@ class GLTableCollectionViewController: UITableViewController, UICollectionViewDa
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
-		// Uncomment the following line to preserve selection between presentations
+		// Uncomment the following line to preserve selection between
+		// presentations
 		// self.clearsSelectionOnViewWillAppear = false
 
-		// Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+		// Uncomment the following line to display an Edit button in the
+		// navigation bar for this view controller.
 		// self.navigationItem.rightBarButtonItem = self.editButtonItem()
 	}
 
@@ -144,9 +146,10 @@ class GLTableCollectionViewController: UITableViewController, UICollectionViewDa
 	}
 
     /*
-    // MARK: - Navigation
+    // MARK: <Navigation>
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    // In a storyboard-based application, you will often want to do a little
+	// preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
