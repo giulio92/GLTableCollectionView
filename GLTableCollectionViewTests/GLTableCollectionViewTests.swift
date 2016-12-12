260a7e2c29a7425b1e42f0ef13edd2a52e4eeaa6
Giulio Lombardo <giulio.lombardo@gmail.com>
2016-12-12 16:51:32 +0100

    - Multiple improvements in GLTableCollectionViewTests, now it's able to test the GLTableCollectionViewController by loading the controller from the UIStoryboard file - Added a new test to check the UITableViewCells class

--- GLTableCollectionViewTests/GLTableCollectionViewTests.swift
+++ GLTableCollectionViewTests/GLTableCollectionViewTests.swift
@@ -10,42 +10,56 @@ import XCTest
 @testable import GLTableCollectionView
 
 class GLTableCollectionViewTests: XCTestCase {
+	var tableCollectionView: GLTableCollectionViewController!
+
 	override func setUp() {
 		super.setUp()
 		// Put setup code here. This method is called before the invocation of
 		// each test method in the class.
+
+		let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+		tableCollectionView = storyboard.instantiateViewController(withIdentifier: "tableCollectionView") as! GLTableCollectionViewController
+		tableCollectionView.beginAppearanceTransition(true, animated: false)
+	}
+
+	func testDataSource() {
+		XCTAssertNotNil(tableCollectionView.tableView.dataSource, "UITableView dataSource is nil")
+	}
+
+	func testDelegate() {
+		XCTAssertNotNil(tableCollectionView.tableView.delegate, "UITableView delegate is nil")
 	}
 
 	func testInstanceVariables() {
-		let controller: GLTableCollectionViewController = GLTableCollectionViewController()
-
-		XCTAssertGreaterThan(controller.numberOfSections, 0, "UITableView must have at least one section")
-		XCTAssertGreaterThan(controller.numberOfCollectionsForRow, 0, "There must be at least a GLIndexedCollectionView per UITableViewCell")
-		XCTAssertGreaterThan(controller.numberOfCollectionItems, 0, "There must be at least one GLIndexedCollectionViewCell")
-		XCTAssertNotEqual(controller.tableCellID, "", "The cellIdentifier for the UITableViewCells must not be empty")
-		XCTAssertTrue(controller.tableCellID.hasSuffix("_section_#"), "The cellIdentifier for the UITableViewCells must end with section number suffix")
-		XCTAssertNotEqual(controller.collectionCellID, "", "The cellIdentifier for the UICollectionCells should not be empty")
+		XCTAssertGreaterThan(tableCollectionView.numberOfSections, 0, "UITableView must have at least one section")
+		XCTAssertGreaterThan(tableCollectionView.numberOfCollectionsForRow, 0, "There must be at least a GLIndexedCollectionView per UITableViewCell")
+		XCTAssertGreaterThan(tableCollectionView.numberOfCollectionItems, 0, "There must be at least one GLIndexedCollectionViewCell")
+		XCTAssertNotEqual(tableCollectionView.tableCellID, "", "The cellIdentifier for the UITableViewCells must not be empty")
+		XCTAssertTrue(tableCollectionView.tableCellID.hasSuffix("_section_#"), "The cellIdentifier for the UITableViewCells must end with section number suffix")
+		XCTAssertNotEqual(tableCollectionView.collectionCellID, "", "The cellIdentifier for the UICollectionCells should not be empty")
 	}
 
 	func testRandomColorsGeneration() {
-		let controller: GLTableCollectionViewController = GLTableCollectionViewController()
-		controller.viewDidLoad()
-
-		let colorsDictionary: [Int: [UIColor]] = controller.colorsDict
+		let colorsDictionary: [Int: [UIColor]] = tableCollectionView.colorsDict
 
 		XCTAssertNotNil(colorsDictionary, "Colors dictionary is nil")
-		XCTAssertEqual(colorsDictionary.count, controller.numberOfSections, "The number of keys in the colors dictionary must match the number of UITableView sections")
+		XCTAssertEqual(colorsDictionary.count, tableCollectionView.numberOfSections, "The number of keys in the colors dictionary must match the number of UITableView sections")
 
-		for colorSection in 0..<controller.numberOfSections {
-			XCTAssertEqual(colorsDictionary[colorSection]!.count, controller.numberOfCollectionItems, "The number of colors for section must match the number of UICollectionCells")
+		for colorSection in 0..<tableCollectionView.numberOfSections {
+			XCTAssertEqual(colorsDictionary[colorSection]!.count, tableCollectionView.numberOfCollectionItems, "The number of colors for section must match the number of UICollectionCells")
+		}
+	}
+
+	func testUITableViewCellsClass() {
+		let visibleCells: [UITableViewCell] = tableCollectionView.tableView.visibleCells
+
+		for cell in visibleCells {
+			XCTAssertTrue(cell is GLCollectionTableViewCell, "UITableViewCells must be GLCollectionTableViewCell")
 		}
 	}
 
 	func testOpaqueFlag() {
-		let controller: GLTableCollectionViewController = GLTableCollectionViewController()
-		controller.viewDidLoad()
-
-		XCTAssertTrue(controller.tableView.isOpaque, "The UITableView should be opaque for increased performances")
+		XCTAssertTrue(tableCollectionView.tableView.isOpaque, "The UITableView should be opaque for increased performances")
 	}
 
 	override func tearDown() {
