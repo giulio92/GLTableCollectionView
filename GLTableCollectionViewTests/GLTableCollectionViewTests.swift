3141bf2dd9e20787f334735a1605c7d76368257f
Giulio Lombardo <giulio.lombardo@gmail.com>
2016-12-12 10:47:26 +0100

    - Added two Unit Tests to test if the GLTableCollectionView is built correctly

--- GLTableCollectionViewTests/GLTableCollectionViewTests.swift
+++ GLTableCollectionViewTests/GLTableCollectionViewTests.swift
@@ -11,14 +11,36 @@ import XCTest
 
 class GLTableCollectionViewTests: XCTestCase {
 	override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of
+		super.setUp()
+		// Put setup code here. This method is called before the invocation of
 		// each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of
+	}
+
+	func testIstantVariables() {
+		let controller: GLTableCollectionViewController = GLTableCollectionViewController()
+
+		XCTAssertGreaterThan(controller.numberOfSections, 0, "UITableView must have at least one section")
+		XCTAssertGreaterThan(controller.numberOfCollectionsForRow, 0, "There should be at least a GLIndexedCollectionView per UITableViewCell")
+		XCTAssertGreaterThan(controller.numberOfCollectionItems, 0, "There should be at least one GLIndexedCollectionViewCell")
+	}
+
+	func testRandomColorsGeneration() {
+		let controller: GLTableCollectionViewController = GLTableCollectionViewController()
+		controller.viewDidLoad()
+
+		let colorsDictionary: [Int: [UIColor]] = controller.colorsDict
+
+		XCTAssertNotNil(colorsDictionary, "Colors dictionary is nil")
+		XCTAssertEqual(colorsDictionary.count, controller.numberOfSections, "The number of keys in the colors dictionary must match the number of UITableView sections")
+
+		for colorSection in 0..<controller.numberOfSections {
+			XCTAssertEqual(colorsDictionary[colorSection]!.count, controller.numberOfCollectionItems, "The number of colors for section must match the number of UICollectionCells")
+		}
+	}
+
+	override func tearDown() {
+		// Put teardown code here. This method is called after the invocation of
 		// each test method in the class.
-        super.tearDown()
-    }
+		super.tearDown()
+	}
 }
