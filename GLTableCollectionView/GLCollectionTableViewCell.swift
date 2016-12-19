018ed0d4c080327f3c6adab64c8e059fc1d4cc9d
Giulio Lombardo <giulio.lombardo@gmail.com>
2016-12-19 11:14:09 +0100

    - Multiple comments updates and grammar fixes

--- GLTableCollectionView/GLCollectionTableViewCell.swift
+++ GLTableCollectionView/GLCollectionTableViewCell.swift
@@ -21,29 +21,31 @@ class GLIndexedCollectionViewFlowLayout: UICollectionViewFlowLayout {
 		}
 
 		// To make paginated scrolling work fine this CGFloat below must be
-		// equal to the value set in the insetForSectionAt method
+		// equal to the value set in the insetForSectionAt method of
 		// UICollectionView's UICollectionViewDelegate Flow Layout.
 		let collectionViewInsets: CGFloat = 10.0
 
 		// Since UICollectionViewFlowLayout proposedContentOffset coordinates
 		// won't take count of any UICollectionView UIEdgeInsets values we need
-		// to fix it by adding collectionViewInsets to .x coordinate.
+		// to fix it by adding collectionViewInsets to the .x coordinate.
 		//
-		// Note: This will cover horizonal scrolling and pagination, if you need
-		// vertical pagination replace the .x coordinate with .y and update
+		// Note: This will only cover horizonal scrolling and pagination, if you
+		// need vertical pagination replace the .x coordinate with .y and update
 		// collectionViewInsets value with the approriate one.
 		let proposedXCoordWithInsets = proposedContentOffset.x + collectionViewInsets
 
-		// We start by creating a variable and we assign a very high CGFloat to
-		// it, a big number here is needed to cover very large
-		// UICollectionViewContentSize cases.
+		// We now create a variable and we assign a very high CGFloat to it (a
+		// big number here is needed to cover very large
+		// UICollectionViewContentSize cases). This var will hold the needed
+		// horizontal adjustment to make the UICollectionView paginate scroll.
 		var offsetCorrection: CGFloat = .greatestFiniteMagnitude
 
 		// Now we loop through all the different layout attributes of the
 		// UICollectionViewCells contained between the .x value of the
-		// proposedContentOffset and collectionView's width looking for the cell
-		// which needs the least offsetCorrection value, it will mean that it's
-		// the first cell on the left of the screen which will give pagination.
+		// proposedContentOffset and collectionView's width, looking for the
+		// cell which needs the least offsetCorrection value: it will mean that
+		// it's the first cell on the left of the screen which will give
+		// pagination.
 		for layoutAttributes in super.layoutAttributesForElements(in: CGRect(x: proposedContentOffset.x, y: 0, width: collectionView!.bounds.width, height: collectionView!.bounds.height))! {
 			// Since layoutAttributesForElements may contain all sort of layout
 			// attributes we need to check if it belongs to a
--- GLTableCollectionView/GLTableCollectionViewController.swift
+++ GLTableCollectionView/GLTableCollectionViewController.swift
@@ -23,7 +23,7 @@ class GLTableCollectionViewController: UITableViewController, UICollectionViewDa
 
 	var colorsDict: [Int: [UIColor]] = [:]
 
-	// Set true to enable UICollectionViews scroll pagination
+	/// Set true to enable UICollectionViews scroll pagination
 	var paginationEnabled: Bool = true
 
 	override func viewDidLoad() {
@@ -72,18 +72,18 @@ class GLTableCollectionViewController: UITableViewController, UICollectionViewDa
 	}
 
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		// As you can see below instead of having a single cellIdentifier for
-		// each type of UITableViewCells, as you would do with normally, we will
-		// have multiple IDs each related to a indexPath section. Doing so the
-		// UITableViewCells will still be recycled but it will be done only with
-		// dequeueReusableCell of a given section.
+		// Instead of having a single cellIdentifier for each type of
+		// UITableViewCells, like in a regular implementation, we will have
+		// multiple cellIDs each related to a precise indexPath section.
+		// Doing so the UITableViewCells will still be recycled but only with
+		// dequeueReusableCell of that section.
 		//
 		// For example the cellIdentifier for section 4 cells will be:
 		// "tableViewCellID_section_#3"
 		// dequeueReusableCell will only reuse previous UITableViewCells with
 		// the same cellIdentifier instead of using any UITableViewCell as a
 		// regular UITableView would do, this is necessary because every cell
-		// will have a different UICollectionView and UICollectionViewCells in
+		// will have a different UICollectionView with UICollectionViewCells in
 		// it and UITableView reuse won't work as expected giving back wrong
 		// cells.
 		var cell: GLCollectionTableViewCell? = tableView.dequeueReusableCell(withIdentifier: tableCellID + indexPath.section.description) as? GLCollectionTableViewCell
