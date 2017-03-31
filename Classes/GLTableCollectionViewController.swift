805a1f531c79e2c3536999595e32940d167bcdf9
Giulio Lombardo <giulio.lombardo@gmail.com>
2017-03-31 16:33:02 +0200

    Very minor layout change

--- Classes/GLTableCollectionViewController.swift
+++ Classes/GLTableCollectionViewController.swift
@@ -212,8 +212,8 @@ class GLTableCollectionViewController: UITableViewController, UICollectionViewDa
 	// In a storyboard-based application, you will often want to do a little
 	// preparation before navigation
 	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-	// Get the new view controller using segue.destinationViewController.
-	// Pass the selected object to the new view controller.
+		// Get the new view controller using segue.destinationViewController.
+		// Pass the selected object to the new view controller.
 	}
 	*/
 }
